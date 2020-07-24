56375945b2f426c18bc0fde9d35af40725b6e86b
JENNY1 <jenny.kim@kinemaster.com>
2020-07-24 14:33:53 +0900

    kotlin 파일으로 변환

---
+++ app/src/main/java/com/reno/myloadingdialog/LoadingDialog.kt
@@ -0,0 +1,21 @@
+package com.reno.myloadingdialog
+
+/**
+ * TODO
+ *
+ *
+ * 1. 배경을 투명하게 만들어 보기
+ * 2. 다이얼 로그 화면의 바깥을 클릭했을 경우 꺼지는 기능 (cancelable) 을 추가 해보기
+ * 3. kotlin 으로 바꿔보기
+ */
+class LoadingDialog(@NonNull context: Context?) : Dialog(context) {
+    init {
+        setContentView(R.layout.loading_dialog)
+        setCancelable(true)
+        setCanceledOnTouchOutside(true)
+        val window: Window = getWindow()
+        if (window != null) {
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+    }
+}
